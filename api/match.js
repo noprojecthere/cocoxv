dc74d6696a5e910d9413fc91791958cfc5e2a9ee
noprojecthere <146555082+noprojecthere@users.noreply.github.com>
2026-02-11 21:43:01 +0530

    Update match.js

--- api/match.js
+++ api/match.js
@@ -53,6 +53,16 @@ module.exports = (req, res) => {
       }
     }
 
+    "test": {
+  "title": "🧪 Test Stream - Big Buck Bunny",
+  "logo": "",
+  "group": "Test",
+  "url": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "type": "hls",
+  "headers": {},
+  "drm": null
+    }
+
     // ✅ NAYA MATCH ADD KARNA HO TO:
     // "match3": {
     //   "title": "Match Name",
